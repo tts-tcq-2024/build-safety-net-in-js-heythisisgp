607e75ec52a24677ad17bac36b7bf1d23613e9ed
ganeshpandi <heythisisgp@gmail.com>
2024-07-07 21:08:40 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,44 +1,38 @@
-const soundexDict = {
-  'B': '1', 'F': '1', 'P': '1', 'V': '1',
-  'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
-  'D': '3', 'T': '3',
-  'L': '4',
-  'M': '5', 'N': '5',
-  'R': '6'
-};
-
 function getSoundexCode(char) {
-  char = char.toUpperCase();
-  return soundexDict[char] || '0';
-}
-
-function filterSoundexCodes(name) {
-  let filteredCodes = [];
-  let prevCode = getSoundexCode(name[0]);
-
-  for (let i = 1; i < name.length && filteredCodes.length < 3; i++) {
-    let code = getSoundexCode(name[i]);
-    if (code !== '0' && code !== prevCode) {
-      filteredCodes.push(code);
-      prevCode = code;
-    }
-  }
-  return filteredCodes;
-}
-
-function padSoundexCodes(codes) {
-  return codes.concat('000').slice(0, 3).join('');
+    char = char.toUpperCase();
+    const soundexDict = {
+        'B': '1', 'F': '1', 'P': '1', 'V': '1',
+        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+        'D': '3', 'T': '3',
+        'L': '4',
+        'M': '5', 'N': '5',
+        'R': '6'
+    };
+    return soundexDict[char] || '0';
 }
 
 function generateSoundex(name) {
-  if (!name) return '';
-  const firstLetter = name[0].toUpperCase();
-  const filteredCodes = filterSoundexCodes(name);
-  const paddedCodes = padSoundexCodes(filteredCodes);
-  return firstLetter + paddedCodes;
+    if (!name) return '';
+
+    let soundex = [name[0].toUpperCase()];
+    let prevCode = getSoundexCode(name[0]);
+
+    for (let i = 1; i < name.length && soundex.length < 4; i++) {
+        let code = getSoundexCode(name[i]);
+        if (code !== '0' && code !== prevCode) {
+            soundex.push(code);
+        }
+        prevCode = code;
+    }
+
+    while (soundex.length < 4) {
+        soundex.push('0');
+    }
+
+    return soundex.join('');
 }
 
 module.exports = {
-  getSoundexCode,
-  generateSoundex
+    getSoundexCode,
+    generateSoundex
 };
