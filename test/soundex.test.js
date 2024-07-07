5bb6a051a29bdff1839bf3e5498b5b4b83deb7d7
ganeshpandi <heythisisgp@gmail.com>
2024-07-07 21:30:22 +0530

    Update soundex.test.js

--- test/soundex.test.js
+++ test/soundex.test.js
@@ -1,33 +1,33 @@
-const { getSoundexCode, generateSoundex } = require('./soundex');
+const soundex = require('./soundex');
 
 describe('Soundex', () => {
   describe('getSoundexCode', () => {
     it('returns the correct soundex code for a given character', () => {
-      expect(getSoundexCode('B')).to.equal('1');
-      expect(getSoundexCode('C')).to.equal('2');
-      expect(getSoundexCode('D')).to.equal('3');
-      expect(getSoundexCode('E')).to.equal('0');
+      expect(soundex.getSoundexCode('B')).to.equal('1');
+      expect(soundex.getSoundexCode('C')).to.equal('2');
+      expect(soundex.getSoundexCode('D')).to.equal('3');
+      expect(soundex.getSoundexCode('E')).to.equal('0');
     });
   });
 
   describe('generateSoundex', () => {
     it('returns an empty string for an empty input', () => {
-      expect(generateSoundex('')).to.equal('');
+      expect(soundex.generateSoundex('')).to.equal('');
     });
 
     it('returns the correct soundex code for a given name', () => {
-      expect(generateSoundex('John')).to.equal('J500');
-      expect(generateSoundex('Mary')).to.equal('M600');
-      expect(generateSoundex('Robert')).to.equal('R163');
+      expect(soundex.generateSoundex('John')).to.equal('J500');
+      expect(soundex.generateSoundex('Mary')).to.equal('M600');
+      expect(soundex.generateSoundex('Robert')).to.equal('R163');
     });
 
     it('ignores vowels and non-alphabetic characters', () => {
-      expect(generateSoundex('John Doe')).to.equal('J500');
-      expect(generateSoundex('Mary-Ann')).to.equal('M600');
+      expect(soundex.generateSoundex('John Doe')).to.equal('J500');
+      expect(soundex.generateSoundex('Mary-Ann')).to.equal('M600');
     });
 
     it('handles names with multiple consecutive consonants', () => {
-      expect(generateSoundex('McDonald')).to.equal('M235');
+      expect(soundex.generateSoundex('McDonald')).to.equal('M235');
     });
   });
 });
