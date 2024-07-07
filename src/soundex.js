14d903b62ed4507d365a6a0c143e2a40be0fa1ca
ganeshpandi <heythisisgp@gmail.com>
2024-07-07 21:32:05 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,31 +1,38 @@
-const soundexDict = {
-  B: '1', F: '1', P: '1', V: '1',
-  C: '2', G: '2', J: '2', K: '2', Q: '2', S: '2', X: '2', Z: '2',
-  D: '3', T: '3',
-  L: '4',
-  M: '5', N: '5',
-  R: '6'
-};
-
 function getSoundexCode(char) {
+  const soundexDict = {
+    'B': '1', 'F': '1', 'P': '1', 'V': '1',
+    'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+    'D': '3', 'T': '3',
+    'L': '4',
+    'M': '5', 'N': '5',
+    'R': '6'
+  };
   return soundexDict[char.toUpperCase()] || '0';
 }
 
 function generateSoundex(name) {
   if (!name) return '';
 
-  const soundex = [name[0].toUpperCase()];
-  let prevCode = getSoundexCode(name[0]);
+  const firstChar = name[0].toUpperCase();
+  let soundex = [firstChar];
+  let prevCode = getSoundexCode(firstChar);
 
   for (let i = 1; i < name.length && soundex.length < 4; i++) {
-    const code = getSoundexCode(name[i]);
+    let code = getSoundexCode(name[i]);
     if (code !== '0' && code !== prevCode) {
       soundex.push(code);
-      prevCode = code;
     }
+    prevCode = code;
   }
 
-  return soundex.join('').padEnd(4, '0');
+  while (soundex.length < 4) {
+    soundex.push('0');
+  }
+
+  return soundex.join('');
 }
 
-module.exports = { getSoundexCode, generateSoundex };
+module.exports = {
+  getSoundexCode,
+  generateSoundex
+};
