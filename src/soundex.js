6e443e4db0243d0887f25035e508ecce0378acbf
ganeshpandi <heythisisgp@gmail.com>
2024-07-15 01:23:40 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,12 +1,13 @@
+const soundexDict = {
+  'B': '1', 'F': '1', 'P': '1', 'V': '1',
+  'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+  'D': '3', 'T': '3',
+  'L': '4',
+  'M': '5', 'N': '5',
+  'R': '6'
+};
+
 function getSoundexCode(char) {
-  const soundexDict = {
-    'B': '1', 'F': '1', 'P': '1', 'V': '1',
-    'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
-    'D': '3', 'T': '3',
-    'L': '4',
-    'M': '5', 'N': '5',
-    'R': '6'
-  };
   return soundexDict[char.toUpperCase()] || '0';
 }
 
@@ -39,6 +40,5 @@ function generateSoundex(name) {
 }
 
 module.exports = {
-  getSoundexCode,
   generateSoundex
 };
